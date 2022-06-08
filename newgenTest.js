de140c872b9b673399e4ff8d70fef853c8c4659a
Alexandr Gette <70770553+alexandrGette@users.noreply.github.com>
2022-06-08 13:42:25 +0300

    add errors processing and range logic

--- newgenTest.js
+++ newgenTest.js
@@ -2,9 +2,9 @@
 let courses = [
     { name: "Courses in England", prices: [0, 100] },
     { name: "Courses in Germany", prices: [500, null] },
-    { name: "Courses in Italy", prices: [100, 200] },
+    { name: "Courses in Italy", prices: [200, 600] },
     { name: "Courses in Russia", prices: [null, 400] },
-    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in China", prices: [600, 250] },
     { name: "Courses in USA", prices: [200, null] },
     { name: "Courses in Kazakhstan", prices: [56, 324] },
     { name: "Courses in France", prices: [null, null] },
@@ -13,7 +13,7 @@ let courses = [
 // Варианты цен (фильтры), которые ищет пользователь
 let requiredRange1 = [null, 200];
 let requiredRange2 = [100, 350];
-let requiredRange3 = [200, null];
+let requiredRange3 = [600, 1000];
 
 
 /**
@@ -23,24 +23,49 @@ let requiredRange3 = [200, null];
  * @returns {*[]} Отфильтрованный список курсов
  */
 function filterCoursesByPriceRange(courses, requiredRange){
-    const coursesArr = [...courses];
-    const requiredRangeMinLimit = requiredRange[0] ? requiredRange[0] : 0;
-    const requiredRangeMaxLimit = requiredRange[1] ? requiredRange[1] : Infinity;
+    try {
+        const coursesArr = [...courses];
+        const requiredRangeMinLimit = requiredRange[0] ? requiredRange[0] : 0;
+        const requiredRangeMaxLimit = requiredRange[1] ? requiredRange[1] : Infinity;
 
-    const filteredCoursesArr = coursesArr.filter((course) => {
-        const courseMinPrice = course.prices[0] ? course.prices[0] : 0;
-        const courseMaxPrice = course.prices[1] ? course.prices[1] : Infinity;
-
-        if ((courseMinPrice <= requiredRangeMaxLimit) && (courseMinPrice >= requiredRangeMinLimit)) {
-            return true
+        if (requiredRangeMaxLimit < requiredRangeMinLimit){
+            throw new SyntaxError("Максимальное значение диапазона не может быть меньше минимального значение диапазона");
         }
-        if ((courseMaxPrice >= requiredRangeMinLimit) && (courseMaxPrice <= requiredRangeMaxLimit)) {
-            return true
-        }
-        return false
-    })
 
-    return filteredCoursesArr
+        const filteredCoursesArr = coursesArr.filter((course) => {
+            const courseMinPrice = course.prices[0] ? course.prices[0] : 0;
+            const courseMaxPrice = course.prices[1] ? course.prices[1] : Infinity;
+
+            try {
+                if (courseMaxPrice < courseMinPrice){
+                    throw new SyntaxError("Максимальная цена курса не может быть меньше минимальной цены");
+                }
+
+                if ((courseMinPrice <= requiredRangeMaxLimit) && (courseMinPrice >= requiredRangeMinLimit)) {
+                    return true
+                }
+                if ((courseMaxPrice >= requiredRangeMinLimit) && (courseMaxPrice <= requiredRangeMaxLimit)) {
+                    return true
+                }
+                if ((requiredRangeMaxLimit >= courseMinPrice) && (requiredRangeMaxLimit <= courseMaxPrice)) {
+                    return true
+                }
+                if ((requiredRangeMinLimit <= courseMaxPrice) && (requiredRangeMinLimit >= courseMinPrice)) {
+                    return true
+                }
+
+                return false
+
+            } catch (e){
+                console.log(e)
+            }
+        })
+
+        return filteredCoursesArr
+    }
+    catch (e) {
+        console.error(e)
+    }
 }
 
 /**
@@ -73,3 +98,5 @@ function sortCourses(courses, order){
 
     return sortableCourses
 }
+
+
